25be93c47177ce0be976364ff4358764a29a8ffe
Jean-Marc Strauven <jms@grazulex.be>
2025-02-26 14:30:14 +0100

    #1 work on the tab

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 import os
+import tkinter.font as tkfont
 from tkinter import ttk
 from tkinter.filedialog import asksaveasfilename, askopenfilename
+
 
 windows = tk.Tk()
 width= windows.winfo_screenwidth()               
@@ -16,22 +18,22 @@ def set_file_path(path):
 
 
 def open_file():
-    path = askopenfilename(filetypes=[('Python Files', '*.py'), ('PHP Files', '*.php')])
+    path = askopenfilename(filetypes=[('All Files', '*.*'), ('PHP Files', '*.php'),('Env Files', '*.env'),('Python Files', '*.py')])
     current_tab = tabControl.index("current")
-    current_editor = str('editor' + str(current_tab))
+    current_editor = editorlist[current_tab]
     with open(path, 'r') as file:
         code = file.read()
-        eval(current_editor).delete('1.0', tk.END)
-        eval(current_editor).insert('1.0', code)
+        current_editor.delete('1.0', tk.END)
+        current_editor.insert('1.0', code)
         set_file_path(path)
         tabControl.tab(current_tab, text=os.path.basename(path))
 
 
 def save_as():
     current_tab = tabControl.index("current")
-    current_editor = str('editor' + str(current_tab))
+    current_editor = editorlist[current_tab]
     if file_path == '':
-        path = asksaveasfilename(filetypes=[('Python Files', '*.py')])
+        path = asksaveasfilename(filetypes=[('All Files', '*.*'), ('PHP Files', '*.php'),('Env Files', '*.env'),('Python Files', '*.py')])
     else:
         path = file_path
     with open(path, 'w') as file:
@@ -40,12 +42,17 @@ def save_as():
         set_file_path(path)
 
 def create_tab():
-    next_tab = 'tab'+str(len(tabControl.tabs()))
-    next_editor = 'editor'+str(len(tabControl.tabs()))
+    index_tabs = len(tabControl.tabs())
+    next_tab = 'tab'+str(index_tabs)
+    next_editor = 'editor'+str(index_tabs)
     next_tab = ttk.Frame(tabControl) 
     tabControl.add(next_tab, text ='new file') 
     next_editor = tk.Text(next_tab)
+    font = tkfont.Font(font=next_editor["font"])
+    tab_width = font.measure(" " * 4)
+    next_editor.config(tabs=tab_width)
     next_editor.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
+    editorlist.append(next_editor)
 
 menu_bar = tk.Menu(windows)
 
@@ -74,14 +81,21 @@ windows.grid_columnconfigure(0, weight=1)
 tabControl = ttk.Notebook(windows) 
 tabControl.grid(row=0, column=0, sticky=tk.W + tk.E + tk.N + tk.S, padx=5, pady=5)
 
+editorlist = []
+
 tab0 = ttk.Frame(tabControl) 
 tabControl.add(tab0, text ='new file') 
 editor0 = tk.Text(tab0)
+font = tkfont.Font(font=editor0["font"])
+tab_width = font.measure(" " * 4)
+editor0.config(tabs=tab_width)
 editor0.pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
-
+editorlist.append(editor0)
 
 code_output = tk.Text(height=5)
 code_output.grid(row=1, column=0, sticky=tk.W + tk.E + tk.N + tk.S, padx=5, pady=5)
 
 windows.mainloop()
+
+
 
